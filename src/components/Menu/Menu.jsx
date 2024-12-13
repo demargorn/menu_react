df6f36bcf02e7f1dcc703e1b9072a3bf7a3bbc3e
Михаил Дорошин <demargorn@gmail.com>
2024-12-13 14:28:37 +0300

    code style fixed

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,26 +1,22 @@
 import './Menu.css';
 
 function Menu({ items, opened = false }) {
-   if (opened) {
-      return (
-         <div className='menu menu-open'>
-            <div className='menu-toggle'>
-               <span></span>
-            </div>
-            <nav>
-               <ul>
-                  {items.map((i) => (
-                     <li key={i.title}>
-                        <a href={i.href}>{i.title}</a>
-                     </li>
-                  ))}
-               </ul>
-            </nav>
+   return opened ? (
+      <div className='menu menu-open'>
+         <div className='menu-toggle'>
+            <span></span>
          </div>
-      );
-   }
-
-   return (
+         <nav>
+            <ul>
+               {items.map((i) => (
+                  <li key={i.title}>
+                     <a href={i.href}>{i.title}</a>
+                  </li>
+               ))}
+            </ul>
+         </nav>
+      </div>
+   ) : (
       <div className='menu'>
          <div className='menu-toggle'>
             <span></span>
